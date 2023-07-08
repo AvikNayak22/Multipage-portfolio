03599e932267dd3d0163e992efd6559870ac23e1
AvikNayak <110925067+AvikNayak22@users.noreply.github.com>
2023-07-08 21:00:29 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+import Contact from "./components/Contact";
+import { Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      {/* <Home />
+      <About />
+      <Skills />
+      <Work />
+      <Contact /> */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import Logo from "../assets/AN.png";
+// import { Link } from "react-scroll";
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#060918] text-[#FFFEF9]">
+      <div>
+        <img src={Logo} alt="Logo Image" style={{ width: "50px" }} />
+      </div>
+
+      {/* menu */}
+      <ul className=" hidden md:flex">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/skills">Skills</a>
+        </li>
+        <li>
+          <a href="/work">Projects</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+      {/* Mobile Menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">
+          <a href="/">Home</a>
+        </li>
+        <li className="py-6 text-4xl">
+          {" "}
+          <a href="/about">About</a>
+        </li>
+        <li className="py-6 text-4xl">
+          <a href="/skills">Skills</a>
+        </li>
+        <li className="py-6 text-4xl">
+          <a href="/work">Projects</a>
+        </li>
+        <li className="py-6 text-4xl">
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
+
+      {/* Social Icons */}
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0A66C2]">
+            <a
+              className="flex justify-between items-center w-full text-gray-200 "
+              href="https://www.linkedin.com/in/avik-nayak-50b667222/"
+              target="_blank"
+            >
+              LinkedIn <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#0E1116]">
+            <a
+              className="flex justify-between items-center w-full text-gray-200 "
+              href="https://github.com/AvikNayak22"
+              target="_blank"
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#EA4335]">
+            <a
+              className="flex justify-between items-center w-full text-gray-200 "
+              href="nayakavik19@gmail.com"
+              target="_blank"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#440CCC]">
+            <a
+              className="flex justify-between items-center w-full text-gray-200 "
+              href="https://documentcloud.adobe.com/gsuiteintegration/index.html?state=%7B%22ids%22%3A%5B%221g65fF1-EERSxxLK3bqJ1aAQ3M8SuicIB%22%5D%2C%22action%22%3A%22open%22%2C%22userId%22%3A%22109482712198933315268%22%2C%22resourceKeys%22%3A%7B%7D%7D"
+              target="_blank"
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
